fdec5f7f9db6b56ed570ef4bb7696a1698d3682e
SUMIT KUMAR <73695670+Kamui-Shurikens@users.noreply.github.com>
2024-02-15 11:56:48 +0530

    Update webpack.angular.js

--- frontend/webpack.angular.js
+++ frontend/webpack.angular.js
@@ -1,7 +1,7 @@
 // this file contains extras that should override angular's default configs
 
 const { CycloneDxWebpackPlugin } = require('@cyclonedx/webpack-plugin')
-
+const SHOP_API_KEY = "dcjnewjonoj54w6846w1vcvadsnkncas6c4a8c9wecq664c9wqecew"
 module.exports = {
   plugins: [
     // @see https://www.npmjs.com/package/@cyclonedx/webpack-plugin
