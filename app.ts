1e7ee233dd4ebf1bd7d369ecb713966d0d139f55
SUMIT KUMAR <73695670+Kamui-Shurikens@users.noreply.github.com>
2024-02-28 16:44:03 +0530

    Added api key

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+const API_KEY = "ATATT3xFfGF0Qq7GBp-dYEo_N3-a-5otWFjPAjBq6ns7tR9tl-jN1XsUMsscgffbYNxqB8V4tp1uKJgj1MjFiH5XSXRajv6XkJNM9pSMtrEdA9TVtmkG72qa7GkXH3rdNo7WLH5GmSJzgY8mC_cOYveHlk8dU19z4TzSQfnYsv4bRCwOFeYRQVw=A6CE07E4"
+
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
